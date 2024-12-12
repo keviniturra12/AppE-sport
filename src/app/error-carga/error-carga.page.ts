d02b5d87861ee0631ccc660ebf7ae4a3a05a8e22
keviniturra12 <166088404+keviniturra12@users.noreply.github.com>
2024-12-12 12:00:25 -0300

    Integracion de animation
    se incorporo la animacion en la page error 404

--- src/app/error-carga/error-carga.page.ts
+++ src/app/error-carga/error-carga.page.ts
@@ -1,35 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationExtras } from '@angular/router';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-error-carga',
   templateUrl: './error-carga.page.html',
   styleUrls: ['./error-carga.page.scss'],
 })
-export class ErrorCargaPage implements OnInit {
+export class ErrorCargaPage {
+  @ViewChild('animationContainer', { read: ElementRef }) animationContainer!: ElementRef;
 
-  constructor(private router: Router) {}
+  constructor(private animationCtrl: AnimationController) {}
 
-  ngOnInit() {
-    // Cualquier lógica inicial necesaria para la página de error
+  ngAfterViewInit() {
+    this.playAnimation();
   }
 
-  // Método para manejar el reintento de carga
+  playAnimation() {
+    const animation = this.animationCtrl
+      .create()
+      .addElement(this.animationContainer.nativeElement)
+      .duration(2000)
+      .iterations(Infinity)
+      .keyframes([
+        { offset: 0, transform: 'scale(1)', opacity: '1' },
+        { offset: 0.5, transform: 'scale(1.1)', opacity: '0.8' },
+        { offset: 1, transform: 'scale(1)', opacity: '1' },
+      ]);
+
+    animation.play();
+  }
+
   retry() {
-    console.log('Reintentando la carga...');
-    // Aquí puedes agregar lógica de reintento específica, como volver a llamar a un servicio.
-    // Por ejemplo:
-    // this.someService.retryLastOperation();
-  }
-
-  // Método para navegar con NavigationExtras
-  navigateWithExtras() {
-    const navigationExtras: NavigationExtras = {
-      queryParams: { errorCode: 404, retry: true },
-      fragment: 'top',
-    };
-
-    console.log('Navegando con extras:', navigationExtras);
-    this.router.navigate(['/home'], navigationExtras);
+    // Aquí puedes implementar la lógica para reintentar la acción
+    console.log('Reintentando cargar...');
   }
 }
